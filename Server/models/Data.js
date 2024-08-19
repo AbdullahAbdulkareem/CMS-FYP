fbe614f30e48bd01f201f3e07434c50608bbb491
AbdullahAbdulkareem <143453249+AbdullahAbdulkareem@users.noreply.github.com>
2024-08-19 23:28:25 +0500

    backend changes

--- Server/models/Data.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose')
-const wilksSchema = new mongoose.Schema({
-    bodyWeight: Number,
-    totalWeight: Number,
-    wilksScore: Number,
-  })
-
-  const DataModel= mongoose.model("userdata", wilksSchema)
-  module.exports=DataModel
