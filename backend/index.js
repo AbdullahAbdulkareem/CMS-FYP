47409ada9f641e0a99adab54cc6104a54ed2bf5f
AbdullahAbdulkareem <143453249+AbdullahAbdulkareem@users.noreply.github.com>
2024-08-21 11:22:26 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,14 +20,8 @@ app.get("/",(req , res)=>{
 // Define the POST route
 app.post('/create_build', (req, res) => {
   const { totalFloors, roomsPerFloor } = req.body;
+  
    res.json({ totalFloors, roomsPerFloor });
-  console.log('Total Floors:', totalFloors);
-  console.log('Rooms Per Floor:', roomsPerFloor);
-
-  const message = `Building data received successfully! Total Floors: ${totalFloors}, Rooms Per Floor: ${JSON.stringify(roomsPerFloor)}`;
-
-  // Send the response
-  res.status(200).send({ message });
 });
 
 
